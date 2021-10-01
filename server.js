b3850d383d565ad0c934dde516fe3108059f4923
asiedusolo <solomon.asiedu@amalitech.org>
2021-09-30 20:27:30 -0400

    making sure allQuotesFormat is correct

--- server.js
+++ server.js
@@ -21,8 +21,11 @@ app.get('/api/quotes/random', (req, res, next) => {
 
 app.get('/api/quotes', (req, res, next) => {
     const quotePerson = req.query.person
+    const allQuotesObj = {
+        quotes: quotes
+    }
     if(!quotePerson){
-        res.send(quotes)
+        res.send(allQuotesObj)
     }else{
         const personQuotes = getAllQuotesByPerson(quotePerson, quotes)
         const allQuotesObj = {
